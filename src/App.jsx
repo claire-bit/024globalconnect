40130002476db575f64915cdde1422e01272fb9c
333IAN <iisavwa333@gmail.com>
2025-06-07 13:58:41 -0600

    Added Logo component and image

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+/* 
 // src/App.jsx
 import Home from './pages/Home'; // adjust if your folder structure is different
 
 function App() {
   return <Home />;
 }
+
+export default App;
+*/
+import React from 'react';
+import Logo from './Logo';
+
+const App = () => {
+  return <Logo />;
+};
 
 export default App;
